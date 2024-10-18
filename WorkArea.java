a09062b79cdb883e7e29019085f7cec749c20def
Luca Ornstil <118486803+ORNSTIL@users.noreply.github.com>
2024-10-17 22:01:47 -0700

    updated workarea which uses blackboard

--- WorkArea.java
+++ WorkArea.java
@@ -2,50 +2,30 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
-import java.util.ArrayList;
-import java.util.List;
 
 public class WorkArea extends JPanel implements MouseListener {
-    private List<Point> clickPositions;
-    private boolean tracking;
+    private Blackboard blackboard;
 
-    public WorkArea() {
-        this.clickPositions = new ArrayList<>();
+    public WorkArea(Blackboard blackboard) {
+        this.blackboard = blackboard;
         addMouseListener(this);
-    }
-
-    public void startTracking() {
-        tracking = true;
-        clickPositions.clear();  // Clear when tracking starts
-    }
-
-    public void stopTracking() {
-        tracking = false;
-    }
-
-    public List<Point> getClickPositions() {
-        // Return a copy of the list to avoid accidental modification
-        return new ArrayList<>(clickPositions);
     }
 
     @Override
     public void mouseReleased(MouseEvent e) {
-        if (tracking) {
+        if (blackboard.isTracking()) {
             Point clickPosition = e.getPoint();
-            clickPositions.add(clickPosition);  // Add new click
+            blackboard.addClick(clickPosition);  // Add click to blackboard
             System.out.println("Mouse released at: " + clickPosition);
         }
     }
 
     @Override
     public void mouseClicked(MouseEvent e) {}
-
     @Override
     public void mousePressed(MouseEvent e) {}
-
     @Override
     public void mouseEntered(MouseEvent e) {}
-
     @Override
     public void mouseExited(MouseEvent e) {}
 }
